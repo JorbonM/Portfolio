e091e2203dfde62957bdfbcada19131542b8e2c9
Jordan Math <jmath0810@gmail.com>
2025-06-27 11:37:16 +1000

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Example from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+      <Example/>
+  )
+}
+
+export default App
