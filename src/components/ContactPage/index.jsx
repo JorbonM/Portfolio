af1a183c1a753e1895088207aeefcd488ea13952
jordan <jmath0810@gmail.com>
2026-08-09 22:17:33 +1000

    adjusted contact page

--- src/components/ContactPage/index.jsx
+++ src/components/ContactPage/index.jsx
@@ -1,72 +1,78 @@
+```jsx
 import { useForm } from "react-hook-form";
-import HCaptcha from '@hcaptcha/react-hcaptcha';
-import './index.scss'
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+import "./index.scss";
 
 export default function ContactForm() {
   const { register, handleSubmit, setValue } = useForm();
-  
+
   const onHCaptchaChange = (token) => {
     setValue("h-captcha-response", token);
   };
-  
+
   const onSubmit = async (data) => {
     console.log(data);
-    
-    await fetch("https://api.web3forms.com/submit", {
+
+    const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
-     'Content-Type': 'application/json',
-        Accept: "application/json"
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      body: JSON.stringify(data)
-    }).then((res) => res.json());
-  }
+      body: JSON.stringify(data),
+    });
 
-return (
-<>
-  <div className="mx-16 my-8 rounded-lg bg-[#587ba3]/50 py-12 backdrop-blur-sm">
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="mb-6 text-center text-6xl font-bold text-[#f0eded]">
-        Contact
-      </h1>
+    const result = await response.json();
 
+    console.log(result);
+  };
+
+  return (
+    <>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col"
-        method='POST'
       >
-        <input type="hidden" name="access_key" value="f62f1850-3440-43a2-bbbd-0d24bf74322f"/>
         <input
+          type="hidden"
+          {...register("access_key")}
+          value="f62f1850-3440-43a2-bbbd-0d24bf74322f"
+        />
+
+        <input
+          {...register("name")}
           type="text"
           id="name"
-          name="name"
           placeholder="Name"
           required
         />
+
         <input
+          {...register("email")}
           type="email"
           id="email"
-          name="email"
           placeholder="Email"
           required
         />
+
         <textarea
+          {...register("message")}
           id="message"
-          name="message"
           placeholder="Write message here :)"
           required
         />
-        <button id="submit" type="submit">
-          Submit Form
-        </button>
-        <br/><br/>
+
         <HCaptcha
           reCaptchaCompat={false}
           onVerify={onHCaptchaChange}
           sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
         />
+
+        <button id="submit" type="submit">
+          Submit Form
+        </button>
       </form>
-    </div>
-  </div>
-</>
-)}
+    </>
+  );
+}
+```
