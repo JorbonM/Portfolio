5e8000bdf0149bd5d8c2a54a0c0a74dabcaa7e1a
jordan <jmath0810@gmail.com>
2026-08-09 22:38:15 +1000

    fixed styling

--- src/components/ContactPage/index.jsx
+++ src/components/ContactPage/index.jsx
@@ -30,42 +30,49 @@ export default function ContactForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-      <input
-        type="hidden"
-        value="f62f1850-3440-43a2-bbbd-0d24bf74322f"
-        {...register("access_key")}
-      />
+    <div className="mx-16 my-8 rounded-lg bg-[#587ba3]/50 py-12 backdrop-blur-sm">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="mb-6 text-center text-6xl font-bold text-[#f0eded]">
+          Contact
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+          <input
+            type="hidden"
+            value="f62f1850-3440-43a2-bbbd-0d24bf74322f"
+            {...register("access_key")}
+          />
 
-      <input
-        type="text"
-        {...register("name")}
-        placeholder="Name"
-        required
-      />
+          <input
+            type="text"
+            {...register("name")}
+            placeholder="Name"
+            required
+          />
 
-      <input
-        type="email"
-        {...register("email")}
-        placeholder="Email"
-        required
-      />
+          <input
+            type="email"
+            {...register("email")}
+            placeholder="Email"
+            required
+          />
 
-      <textarea
-        {...register("message")}
-        placeholder="Write message here :)"
-        required
-      />
+          <textarea
+            {...register("message")}
+            placeholder="Write message here :)"
+            required
+          />
 
-      <HCaptcha
-        sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
-        reCaptchaCompat={false}
-        onVerify={onHCaptchaChange}
-      />
+          <HCaptcha
+            sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+            reCaptchaCompat={false}
+            onVerify={onHCaptchaChange}
+          />
 
-      <button type="submit" id="submit" >
-        Submit Form
-      </button>
-    </form>
+          <button type="submit" id="submit" >
+            Submit Form
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
